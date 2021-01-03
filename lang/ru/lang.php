5d158461136b335192f29c90b23da8c97196935e
seregakasyanow <serega.kasyanow@yandex.ru>
2021-01-03 09:37:33 +0400

    fix license

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -2,7 +2,6 @@
 
 return [
     'config' => [
-        'unknown_type'               => 'Незвестный тип модального окна',
         'content_or_content_partial' => 'Укажите content или contentPartial в конфигурации :definition',
         'unknown_popup_type'         => 'Неизвестный тип модального окна у :definition'
     ],
